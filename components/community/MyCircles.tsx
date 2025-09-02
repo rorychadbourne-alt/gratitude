04e6e3fbab3782c4d9fba53b1f9a2ebc9051022d
rorychadbourne-alt <rorychadbourne@gmail.com>
2025-09-02 15:13:18 +0100

    small fix &apos

--- components/community/MyCircles.tsx
+++ components/community/MyCircles.tsx
@@ -97,7 +97,7 @@ export default function MyCircles({ user, refreshTrigger }: MyCirclesProps) {
       <div className="bg-white rounded-lg shadow-sm p-6">
         <h3 className="text-lg font-semibold text-gray-900 mb-4">My Circles</h3>
         <p className="text-gray-500 text-center py-8">
-          You haven't joined any circles yet. Create one or join using an invite code!
+          You haven&apos;t joined any circles yet. Create one or join using an invite code!
         </p>
       </div>
     )
